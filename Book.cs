7c2f1953c3d5f8cd35e16aa329c8b3b602843200
Meera E <meera.e@intimetec.com>
2021-03-30 19:39:03 +0530

    Assignment1: Management Asset

---
+++ Book.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+
+namespace AssestManagement
+{
+    //Defining a class Book
+    class Book
+    {
+        public int bookId;
+        public string bookName;
+        public string bookCategory;
+        public int x;
+
+  }
+}
